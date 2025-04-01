d81cb8299fa8c20fdad78d96fd5cd05d4206f9f0
jon1476 <aniz.153990@e.unavarra.es>
2025-04-01 13:49:43 +0200

    [] - listaCompraTest subido desde el repo antiguo

--- tests/listaCompraTest.php
+++ tests/listaCompraTest.php
@@ -7,11 +7,42 @@ use PHPUnit\Framework\TestCase;
 
 class listaCompraTest extends TestCase
 {
-    public function givenInstruccionNoValidaReturnsLista(): void
+    /**
+     * @test
+     */
+    public function givenProductoSinCantidadInsertsProductoConCantidad1(): void
     {
         $listaCompra = new listaCompra();
-        $result = $ohce->devolverStringAlReves("hola");
-        $this->assertEquals("aloh", $result);
+        $result = $listaCompra->instruccion("añadir pan");
+        $this->assertEquals("pan x1", $result);
     }
 
+    /**
+     * @test
+     */
+    public function givenProductoConCantidadXToListaVaciaInsertsProductoConCantidadX(): void
+    {
+        $listaCompra = new listaCompra();
+        $result = $listaCompra->instruccion("añadir pan 3");
+        $this->assertEquals("pan x3", $result);
+    }
+    /**
+     * @test
+     */
+    public function givenProductoConCantidadXToListaNoVaciaInsertsProductoConCantidadX(): void
+    {
+        $listaCompra = new listaCompra();
+        $listaCompra->instruccion("añadir pan 3");
+        $result = $listaCompra->instruccion("añadir leche 3");
+        $this->assertEquals("pan x3, leche x3", $result);
+    }
+    /**
+     * @test
+     */
+    public function givenVaciarReturnsListaVacia(): void
+    {
+        $listaCompra = new listaCompra();
+        $result = $listaCompra->instruccion("vaciar");
+        $this->assertEquals("", $result);
+    }
 }
